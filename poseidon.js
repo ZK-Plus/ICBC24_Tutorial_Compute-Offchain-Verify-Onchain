7174a708086abc1bd47187179ccc1c0b5feeac4a
alvaro-alonso <alfmail@pm.me>
2024-05-07 13:09:47 +0200

    basic repo structure

--- poseidon.js
+++
@@ -1,9 +0,0 @@
-
-const circomlibjs = require("circomlibjs");
-
-(async function () {
-    const poseidon = await circomlibjs.buildPoseidon();
-    const hash = poseidon.F.toString(poseidon([1, 2]));
-    console.log(hash);
-
-})()
